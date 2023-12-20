223e53f441b066f4d332ff36ce76cba73c754228
Harshit Vavaiya <harshitpatelhv9@outlook.com>
2023-12-20 14:22:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const repo = 'Harshit-Vavaiya.github.io'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}/`
 
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  distDir:"out"
-}
+
+const nextConfig = {}
 module.exports = nextConfig
